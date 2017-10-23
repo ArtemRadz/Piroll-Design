3d23801ce701595cf86c5f7fe3cebd118233684a
Artem <artemradzivon@gmail.com>
2017-10-23 19:11:22 +0300

    Optimize for IE11

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
-let mobMenuClick = document.querySelector(".mob-menu-click");
-let mobMenuBlock = document.querySelector(".mob-menu-block");
-let mobMenuClose = document.querySelector(".mob-menu-close");
-mobMenuClick.addEventListener('click', (e) => {
+"use strict";
+
+var mobMenuClick = document.querySelector(".mob-menu-click");
+var mobMenuBlock = document.querySelector(".mob-menu-block");
+var mobMenuClose = document.querySelector(".mob-menu-close");
+mobMenuClick.addEventListener('click', function (e) {
 	e.preventDefault();
 	mobMenuBlock.classList.add('mob-menu-block-show');
 	document.body.style.overflow = "hidden";
 });
-mobMenuClose.addEventListener('click', () => {
+mobMenuClose.addEventListener('click', function () {
 	mobMenuBlock.classList.remove('mob-menu-block-show');
 	document.body.style.overflow = "";
-})
+});
 
-let mobMenuListUlA = document.querySelector(".mob-menu-list-ul");
-mobMenuListUlA.addEventListener('click', function(e) {
+var mobMenuListUlA = document.querySelector(".mob-menu-list-ul");
+mobMenuListUlA.addEventListener('click', function (e) {
 	e.preventDefault();
-	let elementTopMenu = e.target.classList.contains("mob-menu-list-ul-top");
-	let back = e.target.classList.contains("mob-menu-list-ul-back");
-	if(elementTopMenu) {
+	var elementTopMenu = e.target.classList.contains("mob-menu-list-ul-top");
+	var back = e.target.classList.contains("mob-menu-list-ul-back");
+	if (elementTopMenu) {
 		this.classList.add("mob-menu-hide");
 		e.target.nextElementSibling.classList.remove("mob-menu-inner-hide");
 	}
-	if(back) {
+	if (back) {
 		this.classList.remove("mob-menu-hide");
 		e.target.closest('ul').classList.add('mob-menu-inner-hide');
 	}
